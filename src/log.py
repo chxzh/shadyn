9dde27feaf3b9e37b83c30e5fb8188ded635dd30
chxzh <418316606@qq.com>
2016-01-27 11:07:50 -0500

    a basic log decorator

--- src/log.py
+++ src/log.py
@@ -3,6 +3,24 @@ import logging
 logging.basicConfig()
 _logger = logging.getLogger(name)
 
-def energy_log(energy):
+def energy_log(energy_func):
     # decorator for energy function in optimization
-    def inner(self, x):
+    def inner(optimizer, x): # x is expected to be a 1-d numpy array
+        res = energy_func(x)
+        _logger.info("evaluating on x:{x} -> {}".format(x=x))
+        return res
+    return inner
+
+def optimtask_log(run):
+    # decorator for run() in optimizer
+    def inner(optimizer):
+        _logger.info("starting optimization with configuration of:")
+        _logger.info("method - {name}", name=optimizer._method_name)
+        _logger.info("energies:")
+        for func, name, weight in optimizer._energy_list:
+            # TODO: bad accessing, needs refactoring
+            _logger.info("energy function - {name}, weight - {weight}",
+                         name=name, weight=weight)
+        run()
+        return
+    return inner
