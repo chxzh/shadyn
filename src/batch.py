eb9a25c0a15d97fe0d5d5cf839dc3752ce9979d2
chxzh <418316606@qq.com>
2016-02-03 13:34:23 -0500

    added helper functions

--- src/batch.py
+++ src/batch.py
@@ -1,6 +1,7 @@
 import os, plotting, log
 from shadow_optim import Renderer, Optimizer
 from datetime import datetime as dt
+from itertools import product
 def get_fname(root=None):
     if not root:
         root = os.getcwd()
@@ -10,24 +11,52 @@ def get_fname(root=None):
     path = root + '\\' + time_stamp_str
     if not os.path.exists(path):
         os.mkdir(path)
-    print path
-    return path + '\\' + time_stamp_str
+    return path, time_stamp_str
 
+def get_renderer():
+    renderer = Renderer()
+    renderer.start()
+    renderer.wait_till_init()
+    return renderer
+
+def get_default_plotted_optimizer():    
+    optimizer = Optimizer()
+    plotting.attach_plotter(optimizer, plotting.Plotter(*get_fname("..\\res")))
+    return optimizer
+    
+def single_energy_combo(method_name, energy_name):
+    renderer = get_renderer()
+    
+def all_single_energy_combos():
+    renderer = Renderer()
+    renderer.start()
+    renderer.wait_till_init()
+    x_0 = renderer.get_param()
+    for method_name, energy_name in product(Optimizer.method_dic, Optimizer.energy_dic):
+        print method_name, energy_name
+        renderer.set_param(x_0)
+        optimizer = Optimizer(renderer)
+        plotting.attach_plotter(optimizer, plotting.Plotter(*get_fname("..\\res")))
+        optimizer.set_target("C:\\Users\\cxz\\Pictures\\target.png")
+        optimizer.set_method(method_name)
+        optimizer.set_energy([energy_name], [1])
+        optimizer.run()
+    
 def vanilla():
     renderer = Renderer()
     renderer.start()
     renderer.wait_till_init()
-    plotter = plotting.Plotter(get_fname("..\\res"))
+    plotter = plotting.Plotter(*get_fname("..\\res"))
     optimizer = Optimizer(renderer)
     plotting.attach_plotter(optimizer, plotter)
     optimizer.set_target("C:\\Users\\cxz\\Pictures\\target.png")
-    optimizer.set_method("Powell")
+    optimizer.set_method("BFGS")
     optimizer.set_energy(["first moments (normalized)"], [1])
     optimizer.run()
 
 def main():
-#     os.mkdir("..\\res\\now")
-    vanilla()
+    single_energy()
+#     vanilla()
     pass
 
 if __name__ == "__main__":
